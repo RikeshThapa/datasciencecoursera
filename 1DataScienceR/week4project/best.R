bd622652ffe299a9cf7497686481837ed2030e3d
rthapa@u.rochester.edu <rthapa@u.rochester.edu>
2016-09-27 12:23:49 -0700

    completion of the best function, returns character vector with name of the hospital that has the best

--- 1DataScienceR/week4project/best.R
+++ 1DataScienceR/week4project/best.R
@@ -1,35 +1,49 @@
 # Script Function: Finding the best hospital in a state
 # Function name: best
-# Specs: 
-#     - arguments: 
+# Specifications:
+#     - arguments:
 #         1) the 2-character abbreviated name of a state
 #         2) outcome name
-# Data Source: outcome-of-care-measures.csv 
-# Return: 
-#     - returns character vector with name of the hospital that has the best 
-#       (i.e. lowest) 30-day mortality for the speci???ed outcome in that state. 
-#       The hospital name is the name provided in the Hospital.Name variable. 
+# Data Source: outcome-of-care-measures.csv
+# Return:
+#     - returns character vector with name of the hospital that has the best
+#       (i.e. lowest) 30-day mortality for the specified outcome in that state.
+#       The hospital name is the name provided in the Hospital.Name variable.
 #       + The outcomes can be one of:
-#         -"heart attack", 
-#         -"heart failure", 
-#         - "pneumonia". 
-# Hospitals that do not have data on a particular outcome should be excluded from the set of hospitals when deciding the rankings.
-# Handling ties. If there is a tie for the best hospital for a given outcome, then the hospital names should be sorted in alphabetical order and the ???rst hospital in that set should be chosen (i.e. if hospitals "b", "c", and "f" are tied for best, then hospital "b" should be returned).
-# The function should use the following template.
+#         -"heart attack"
+#         -"heart failure"
+#         -"pneumonia"
+# Corner Cases:
+#     - Hospitals that do not have data on a particular outcome should be excluded
+#       from the set of hospitals when deciding the rankings
+#     - Handling ties: If there is a tie for the best hospital for a given outcome,
+#       then the hospital names should be stored in alphabetical order and the first
+#       hospital in that set should be chosen (i.e. if hospitals "b", "c", and "f"
+#       are tied for best, then hospital "b" should be returned)
 
-best <- function(state, outcome) { ## Read outcome data
-   ## Check that state and outcome are valid
-   ## Return hospital name in that state with lowest 30-day death ## rate
+#Notes: We are looking at column 11 and 19
+best <- function(state, outcome) { 
+  ## Read outcome data
+  outcomeData <- read.csv("outcome-of-care-measure", colClasses = "character")
+  ## Check that state and outcome are valid
+  statesList<-unique(outcomeData$State)
+  try(if(!(state%in%statesList)) stop("invalid state"))
+  possibleOutcomes <- c("heart attack","heart failure","pneumonia")
+  try(if(!(outcome%in%possibleOutcomes))stop("invalid outcome"))
+  newDf <- subset(outcomeData[outcomeData$State == state , ])
+  ## Return hospital name in that state with lowest 30-day death rate
+  formatOutcome<-simpleCap(outcome)
+  formatOutcome<-paste("Hospital.30.Day.Death..Mortality..Rates.from", "Heart.Failure",sep=".")
+  #remove all the NA valued data
+  #newDf<- complete.cases(newDf[, formatOutcome])
+  results <-newDf[which(newDf[formatOutcome] == min(newDf[formatOutcome], na.rm = TRUE)), ]
+  hospitalNames <-sort(results$Hospital.Name)
+  hospitalNames[1]
+
 }
-# The function should check the validity of its arguments. If an invalid state value is passed to best, the function should throw an error via the stop function with the exact message "invalid state". If an invalid outcome value is passed to best, the function should throw an error via the stop function with the exact message "invalid outcome".
-# Here is some sample output from the function.
-# > source("best.R") > best("TX", "heart attack")
-# [1] "CYPRESS FAIRBANKS MEDICAL CENTER"
-# > best("TX", "heart failure")
-# [1] "FORT DUNCAN MEDICAL CENTER"
-# > best("MD", "heart attack")
-# [1] "JOHNS HOPKINS HOSPITAL, THE"
-# > best("MD", "pneumonia")
-# [1] "GREATER BALTIMORE MEDICAL CENTER"
-# > best("BB", "heart attack") Error in best("BB", "heart attack") : invalid state > best("NY", "hert attack") Error in best("NY", "hert attack") : invalid outcome >
-#   Save your code for this function to a ???le named best.R.
+
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=".")
+}
